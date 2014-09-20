66d2ea1527c10ab4c4f8655e7cc924f05f8a6613
peterzaur <liu.peter.j@gmail.com>
2014-09-20 18:54:42 -0400

    added postgres

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 MAIL_SERVER = 'smtp.gmail.com'
