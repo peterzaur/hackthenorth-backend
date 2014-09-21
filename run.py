c2c9c07dda78e07b2b4f29565cd563a559fcf738
peterzaur <liu.peter.j@gmail.com>
2014-09-21 06:09:30 -0400

    cool

--- run.py
+++ run.py
@@ -9,15 +9,15 @@ db = flask.ext.sqlalchemy.SQLAlchemy(app)
 # Create Flask-SQLAlchemy models
 class Users(db.Model):
    id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String)
-   message = db.Column(db.String)
+   email = db.Column(db.String)
+   password = db.Column(db.String)
 
 # Create db tables
 db.create_all()
 
 # Init Flask-Restless API manager, create endpoint (at <host>/api/<tablename>)
 manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Users, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Users, methods=['GET', 'POST'])
 
 # Start flask
 if __name__ == '__main__':
