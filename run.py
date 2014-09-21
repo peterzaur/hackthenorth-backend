90881e8b3c12247ebf1b6bf9b223bcbd3a6a2c20
peterzaur <liu.peter.j@gmail.com>
2014-09-20 22:45:22 -0400

    arg

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
-#!flask/bin/python
-import os
-from app import app
+import flask
+import flask.ext.sqlalchemy
+import flask.ext.restless
+
+app = flask.Flask(__name__)
+app.config.from_object('config')
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
+
+# Create Flask-SQLAlchemy models
+class Users(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String)
+   message = db.Column(db.String)
+
+# Create db tables
+db.create_all()
+
+# Init Flask-Restless API manager, create endpoint (at <host>/api/<tablename>)
+manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+manager.create_api(Users, methods=['GET', 'POST', 'DELETE'])
+
+# Start flask
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug = True, host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    app.run(debug = True)
