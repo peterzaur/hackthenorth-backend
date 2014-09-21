0f125e21da135730dc97a27163837aea26e80d3b
peterzaur <liu.peter.j@gmail.com>
2014-09-20 22:28:58 -0400

    trying to move the app structure around

--- run.py
+++ run.py
@@ -1,23 +1,4 @@
-import flask
-import flask.ext.sqlalchemy
-import flask.ext.restless
-
-app = flask.Flask(__name__)
-app.config.from_object('config')
-db = flask.ext.sqlalchemy.SQLAlchemy(app)
-
-# Create Flask-SQLAlchemy models
-class Users(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String)
-   message = db.Column(db.String)
-
-# Create db tables
-db.create_all()
-
-# Init Flask-Restless API manager, create endpoint (at <host>/api/<tablename>)
-manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Users, methods=['GET', 'POST', 'DELETE'])
-
-# Start flask
-app.run()
+#!flask/bin/python
+from app import app
+if __name__ == '__main__':
+    app.run(debug = True)
