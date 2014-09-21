515bde0732fc8242be86c60531db749d0af9b72a
peterzaur <liu.peter.j@gmail.com>
2014-09-21 09:43:21 -0400

    almost

--- run.py
+++ run.py
@@ -2,19 +2,19 @@ import flask
 import flask.ext.sqlalchemy
 import flask.ext.restless
 import json
-import os
-import psycopg2
-import urlparse
+# import os
+# import psycopg2
+# import urlparse
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+# urlparse.uses_netloc.append("postgres")
+# url = urlparse.urlparse(os.environ["DATABASE_URL"])
+# conn = psycopg2.connect(
+   # database=url.path[1:],
+   # user=url.username,
+   # password=url.password,
+   # host=url.hostname,
+   # port=url.port
+# )
 
 app = flask.Flask(__name__)
 app.config.from_object('config')
