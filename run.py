096b50ee8c837a935ae659f45ae0e95671e89e27
peterzaur <liu.peter.j@gmail.com>
2014-09-21 08:02:19 -0400

    doing this post thing

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ manager.create_api(Users, methods=['GET', 'POST', 'PUT'])
 
 @app.route('/getemail', methods=['POST'])
 def getEmail():
+    print(flask.request.method)
     email = flask.request.form['email']
     print(email)
     print("reached here")
